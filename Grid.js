028ca11fe44e8ee206dddc2e26771d5c7a84ceb1
Jordi <jordi.bonastre.m@gmail.com>
2021-10-09 23:24:22 +0200

    generate grid function

--- Grid.js
+++ Grid.js
@@ -3,13 +3,17 @@ class Grid{
   ctx = canvas.getContext('2d');
 
   constructor(width,height,resolution){
-
     this.width = width;
     this.height = height;
     this.resolution = resolution;
     this.columns = canvas.width / resolution;
     this.rows = canvas.height / resolution;
+  }
 
+  generateGrid(){
+    return new Array(COLS).fill(null)
+    .map(() => new Array(ROWS).fill(null)
+      .map(() => Math.floor(Math.random() * 2)));
   }
 
 
